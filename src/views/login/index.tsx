944bf6dfd9bd1f6568d68465565088ad079b3a29
lilian <1220795758@qq.com>
2023-01-12 11:19:19 +0800

    2023-01-12:登录界面小人模型功能实现

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react"
+import React, { memo, useRef, useState } from "react"
 import type { FC, ReactNode } from "react"
 import style from "./style.module.less"
 
@@ -7,8 +7,60 @@ interface IProps {
 }
 
 const Login: FC<IProps> = () => {
+  /*开始*----------logo小人的逻辑代码----------*/
+  //#region
+
+  // 状态
+  const loginRef = useRef<HTMLDivElement>(null)
+  const [position, setPosition] = useState(10)
+
+  // 功能函数
+  const modelAction = (
+    element: HTMLElement,
+    leftDistance: number,
+    rightDistance: number,
+    actionDistance: number
+  ) => {
+    let flag = 1
+    if (leftDistance >= actionDistance + 10 && rightDistance >= actionDistance + 10) {
+      flag = Math.random() > 0.5 ? -1 : 1
+    } else if (leftDistance < actionDistance + 10) {
+      flag = -1
+    }
+    setPosition(() => {
+      const newPosition = position + flag * actionDistance
+      element.style.right = `${newPosition}px`
+      return newPosition
+    })
+  }
+
+  // 事件处理
+  const handleMouseEnter = (element: HTMLElement) => {
+    if (loginRef.current) {
+      // 舞台宽度
+      const stageWidth = loginRef.current.offsetWidth
+      // 小人自身的宽度
+      const modelWidth = element.offsetWidth
+      // 小人距离舞台左边的距离
+      const leftDistance = element.offsetLeft
+      // 小人距离舞台右边的距离
+      const rightDistance = stageWidth - leftDistance - modelWidth
+      modelAction(element, leftDistance, rightDistance, 100)
+    }
+  }
+
+  //#endregion
+  /*结束*----------logo小人的逻辑代码----------*/
+
   return (
-    <div className={style.login}>
+    <div className={style.login} ref={loginRef}>
+      {/* logo小人 */}
+      <div
+        className={style["login-logo"]}
+        onMouseEnter={(event) => {
+          handleMouseEnter(event.target as HTMLElement)
+        }}
+      ></div>
       <div className={style["login-box"]}>
         <h2>管理员登录</h2>
         <div className={style["user-box"]}>
